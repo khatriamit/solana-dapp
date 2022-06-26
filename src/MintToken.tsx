ce7492438f8c5ffe50c4c06ef0b45880ce6155f5
khatriamit <rajukhatri054@gmail.com>
2022-06-26 17:33:40 +0545

    add token mint balance check and send token functions

--- src/MintToken.tsx
+++ src/MintToken.tsx
@@ -19,11 +19,12 @@ import {
 window.Buffer = window.Buffer || require("buffer").Buffer;
 
 function MintToken() {
+
     const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
     const fromWallet = Keypair.generate();
     let mint: PublicKey;
     let fromTokenAccount: Account;
-
+    const toWallet = new PublicKey("BUDKu4JWSgvYf4LUQH513or15dVNSJZgBvAXEmc6wkRG");
     async function createToken() {
         const formAirdropSignature = await connection.requestAirdrop(
             fromWallet.publicKey,
@@ -47,17 +48,53 @@ function MintToken() {
             fromWallet.publicKey,
         );
         console.log(`Create token account: ", ${fromTokenAccount.address.toBase58()}`);
+    }
+    async function mintToken() {
+        const signature = await mintTo(
+            connection,
+            fromWallet,
+            mint,
+            fromTokenAccount.address,
+            fromWallet.publicKey,
+            10000000000
+        );
+        console.log(`Mint signature: ${signature}`);
+    }
 
+    async function checkBalance() {
+        const mintInfo = await getMint(connection, mint);
+        console.log(mintInfo.supply);
 
+        const tokenAccountInfo = await getAccount(connection, fromTokenAccount.address);
+        console.log(tokenAccountInfo.amount);
+    }
+    async function sendToken() {
+        const toTokenAccount = await getOrCreateAssociatedTokenAccount(
+            connection,
+            fromWallet,
+            mint,
+            toWallet
+        );
+        console.log(`toTokenAccount ${toTokenAccount.address}`);
+
+        const signature = await transfer(
+            connection,
+            fromWallet,
+            fromTokenAccount.address,
+            toTokenAccount.address,
+            fromWallet.publicKey,
+            1000000000,
+        );
+        console.log(`finished transfer with ${signature}`);
     }
     return (
         <div>
             Mint Token Section
             <div>
                 <button onClick={createToken}>Create Token</button>
-                <button>Mint TOken</button>
-                <button>Check Balance</button>
-                <button>Send Token</button>
+                <button onClick={mintToken}>Mint Token</button>
+                <button onClick={checkBalance}>Check Balance</button>
+                <button onClick={sendToken}>Send Token</button>
             </div>
         </div>
     );
